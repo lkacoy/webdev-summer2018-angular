840b70accfc8705c34f6d0f88137ec56dd766540
lkacoy <lkacoyannakis@gmail.com>
2018-07-09 11:12:45 -0400

    add component and add some click events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {HelloWorldComponent} from './hello-world.component';
+import {HelloWorldComponent} from './hello-world/hello-world.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent
+
   ],
   imports: [
     BrowserModule
