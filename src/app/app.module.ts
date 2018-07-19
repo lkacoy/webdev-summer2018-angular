6d6e19554acba33b73befb9bc177a8f15f7a7e18
lkacoy <lkacoyannakis@gmail.com>
2018-07-19 19:18:17 -0400

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import {HelloWorldComponent} from './hello-world/hello-world.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import {FormsModule} from "@angular/forms";
 import {CourseNavigatorServiceClient} from "./services/course-navigator.service.client";
 import { CourseNavigatorComponent } from './course-navigator/course-navigator.component';
 import { WhiteBoardComponent } from './white-board/white-board.component';
-import {CourseServiceClient} from "./services/course.service.client";
 import { CourseGridComponent } from './course-grid/course-grid.component';
+import {CourseServiceClient} from "./services/course.service.client";
 import {routing} from "./app.routing";
 import { CourseViewerComponent } from './course-viewer/course-viewer.component';
 import { ModuleListComponent } from './module-list/module-list.component';
 import {ModuleServiceClient} from "./services/module.service.client";
 import { LessonTabsComponent } from './lesson-tabs/lesson-tabs.component';
 import {LessonServiceClient} from "./services/lesson.service.client";
-import { LoginComponent } from './login/login.component';
-import { ProfileComponent } from './profile/profile.component';
-import { RegisterComponent } from './register/register.component';
-import { SectionListComponent } from './section-list/section-list.component';
 import { WidgetListComponent } from './widget-list/widget-list.component';
+import {WidgetServiceClient} from "./services/widget.service.client";
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import {UserServiceClient} from "./services/user.service.client";
+import { SectionListComponent } from './section-list/section-list.component';
+import {SectionServiceClient} from "./services/section.service.client";
 
 @NgModule({
   declarations: [
@@ -31,21 +34,26 @@ import { WidgetListComponent } from './widget-list/widget-list.component';
     CourseViewerComponent,
     ModuleListComponent,
     LessonTabsComponent,
+    WidgetListComponent,
     LoginComponent,
-    ProfileComponent,
     RegisterComponent,
-    SectionListComponent,
-    WidgetListComponent
-
+    ProfileComponent,
+    SectionListComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     routing
   ],
-  providers: [CourseNavigatorServiceClient,
+  providers: [
+    CourseNavigatorServiceClient,
     CourseServiceClient,
     ModuleServiceClient,
-    LessonServiceClient],
+    LessonServiceClient,
+    WidgetServiceClient,
+    UserServiceClient,
+    SectionServiceClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
