9ef5f087f06bc1fc270c3862b03f9acad3fcb39d
lkacoy <lkacoyannakis@gmail.com>
2018-07-19 18:57:27 -0400

    add register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {UserServiceClient} from "../services/user.service.client";
 
 @Component({
   selector: 'app-register',
@@ -7,7 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,
+              private service: UserServiceClient) { }
+
+  username;
+  password;
+  password2;
+  register(username, password, password2) {
+    this.service
+      .createUser(username, password)
+      .then(() =>
+        this.router.navigate(['profile']));
+  }
 
   ngOnInit() {
   }
