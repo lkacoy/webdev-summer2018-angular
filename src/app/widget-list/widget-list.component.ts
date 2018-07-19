dc84451e91851f6a3b314db17b0ca2911c5794f6
lkacoy <lkacoyannakis@gmail.com>
2018-07-19 18:59:43 -0400

    add widget component

--- src/app/widget-list/widget-list.component.ts
+++ src/app/widget-list/widget-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {WidgetServiceClient} from "../services/widget.service.client";
 
 @Component({
   selector: 'app-widget-list',
@@ -7,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WidgetListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: WidgetServiceClient,
+              private route: ActivatedRoute) {
+    this.route.params.subscribe(params => this.setContext(params));
+  }
+
+  context;
+  widgets = [];
+  setContext(params) {
+    this.context = params;
+    this.loadWidgets(params.lessonId);
+  }
+  loadWidgets(lessonId) {
+    this.service.findWidgetsForLesson(lessonId)
+      .then(widgets => this.widgets = widgets);
+  }
 
   ngOnInit() {
   }
