98eca6b10278097efa6721d47707da978c4dec1e
lkacoy <lkacoyannakis@gmail.com>
2018-07-21 11:56:51 -0400

    fix spacing

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {User} from "../models/user.model.client";
 import {UserServiceClient} from "../services/user.service.client";
 import {Router} from "@angular/router";
 import {SectionServiceClient} from "../services/section.service.client";
