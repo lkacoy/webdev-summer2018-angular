169d454954a59256111541e434a4df015d571778
lkacoy <lkacoyannakis@gmail.com>
2018-07-29 22:15:12 -0400

    deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
-app.use(express.static('./dist/webdev-summer2018-angular'));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/webdev-summer2018-angular/index.html'));
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
